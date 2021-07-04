a0ce02f0fba2672b225de162f28fa34f52bb703f
Colin Nebula <c.nebula@Mac.local>
2021-07-04 17:29:00 -0400

    added js

---
+++ assets/js/game.js
@@ -0,0 +1,23 @@
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+window.alert("This is an alert! JavaScript is running!");
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+
+
+fight();
+
+
+//window.alert("This is an alert! JavaScript is running!");
